50a184cb2c622a796508e545a4c673f7b79e1278
Delinet <candela.antonio@gmail.com>
2013-05-08 23:35:55 +0300

    Update timeline_test1.py

--- timeline_test1.py
+++ timeline_test1.py
@@ -1,10 +1,2 @@
-#!/usr/bin/env python
-#
-# Copyright 2013 Antonio Candela
-#
-# Author: Antonio Candela <candela.antonio@gmail.com>
-# AKA: Delinet <twitter.com/delinet>
-# Web Site: www.snacktech.it
-#
 
 
